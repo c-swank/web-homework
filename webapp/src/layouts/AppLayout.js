544748015de05791862e0abb11ffe9e77b3c8569
chandler3 <chandler@solvvy.com>
2021-01-22 02:37:04 -0700

    Add routing to the new Home Page and the pre-built/provided Home page Remove unused file

--- webapp/src/layouts/AppLayout.js
+++ webapp/src/layouts/AppLayout.js
@@ -2,6 +2,8 @@ import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { MainLayout } from './Main/MainLayout'
 import TransactionsPage from '../routes/Transactions/TransactionsPage'
+import { Home } from '../home/home-page'
+import HomePage from '../routes/Home/HomePage'
 
 export function AppLayout () {
   return (
@@ -9,9 +11,14 @@ export function AppLayout () {
       <Suspense>
         <MainLayout>
           <Switch>
-            <Route exact path='/' />
+            <Route exact path='/'>
+              <HomePage />
+            </Route>
             <Route path='/transactions'>
               <TransactionsPage />
+            </Route>
+            <Route path='/prebuilt-home'>
+              <Home />
             </Route>
           </Switch>
         </MainLayout>
--- webapp/src/layouts/layoutConstants.js
+++
@@ -1,1 +0,0 @@
-export const navBarWidth = '50px'
