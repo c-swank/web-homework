7caa6634d547a73b0b3e19757df7b281546173d9
chandler3 <chandler@solvvy.com>
2021-01-22 02:39:00 -0700

    Add a Container to help with spacing a little better against the permanent navbar Implement a class structure for convenience of state management and code cleanliness (which needs additional cleaning)

--- webapp/src/layouts/Main/MainLayout.js
+++ webapp/src/layouts/Main/MainLayout.js
@@ -1,16 +1,34 @@
 import React from 'react'
+import { Container } from '@material-ui/core'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { css } from '@emotion/core'
 import { PropTypes } from 'prop-types'
 import { NavBar } from './NavBar'
 
-export function MainLayout (props) {
-  return (
-    <div className='main-container' style={{ height: '100vh', overflow: 'auto' }}>
-      <NavBar />
-      <div style={{ display: 'inline-block' }}>
-        {props.children}
+const MainContentContainerStyle = css`
+  margin-top: 15px;
+  padding-left: 50px;
+`
+
+export class MainLayout extends React.Component {
+  render () {
+    return (
+      <div
+        className='main-container'
+        style={{
+          overflow: 'auto',
+          paddingLeft: 0,
+          paddingRight: 0
+        }}
+      >
+        <CssBaseline />
+        <NavBar />
+        <Container css={MainContentContainerStyle}>
+          {this.props.children}
+        </Container>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 MainLayout.propTypes = {
--- webapp/src/layouts/Main/NavBar.js
+++ webapp/src/layouts/Main/NavBar.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { css } from '@emotion/core'
 import { Drawer, Icon, IconButton } from '@material-ui/core'
-import { Close, Home, Menu, Receipt, VideogameAsset } from '@material-ui/icons'
+import { Home, Receipt, Settings, VideogameAsset } from '@material-ui/icons'
 import { bool, PropTypes, string } from 'prop-types'
-import { navBarWidth } from '../layoutConstants'
+import { merge } from 'lodash'
 // Icon courtesy of https://www.freepik.com
 import WizardHatLogo from '../../components/icons/wizard-hat.png'
+import { NestedNavLink } from '../NestedNavigationMenu/NestedNavLink'
 
 const DrawerNavLink = (props) => {
   return (
-    <NavLink activeClassName='active-route' exact={props.exact} to={props.to}>
+    <NavLink activeClassName='active-route' className='route' exact={props.exact} to={props.to}>
       {props.children}
     </NavLink>
   )
@@ -23,69 +24,164 @@ DrawerNavLink.propTypes = {
 }
 
 const NavBarStyle = css`
-  width: ${navBarWidth};
+  .drawer {
+    max-width: 50px;
+  }
+  
+  .nested-drawer {
+    display: none;
+  }
+  
+  .nested-drawer-open {
+    margin-left: 49px;
+    display: block;
+    background-color: #799c9b !important;
+  }
 
   .active-route {
     background-color: #dbdbdb !important;
   }
-`
-
-const OpenDrawerIconStyle = css`
-  position: fixed !important;
-`
-
-export function NavBar () {
-  const [state, setState] = React.useState({
-    open: false
-  })
-
-  const toggleDrawer = (open) => {
-    setState({ open })
+  
+  .active-menu-item {
+    background-color: #799c9b !important;
   }
+  
+  .bottom-nav-container {
+    position: absolute;
+    bottom: 0;
+  }
+  
+  .nested-navigation-container {
+    margin-top: 75px;
+  }
+  
+  .nested-navigation-links {
+    text-align: center;
+  }
+`
 
-  const closeDrawer = () => {
-    toggleDrawer(false)
+const EmptyNavItem = (props) => {
+  const getClassName = () => {
+    const { pathname } = window.location
+    let className = props.className || ''
+    if (props.activeClassName && props.to) {
+      className += new RegExp(props.to).test(pathname) ? props.activeClassName : ''
+    }
+    return className
   }
 
   return (
-    <div>
-      <IconButton css={OpenDrawerIconStyle} onClick={() => { toggleDrawer(true) }}>
-        <Menu />
-      </IconButton>
-      <Drawer anchor='left' css={NavBarStyle} open={state.open}>
-        <IconButton onClick={closeDrawer} >
-          <Close />
-        </IconButton>
-        <Icon css={css`
-        width: 100% !important;
-        height: 50px !important;
-        padding-bottom: 100px !important;
-      `}>
-          <img
-            alt='Yer a Harry Wizard'
-            css={css`
-              padding: 12px !important;
-            `}
-            src={WizardHatLogo}
-            width={25}
-          />
-        </Icon>
-        <DrawerNavLink exact to='/'>
-          <IconButton onClick={closeDrawer}>
-            <Home />
-          </IconButton>
-        </DrawerNavLink>
-        <DrawerNavLink to='/transactions'>
-          <IconButton onClick={closeDrawer}>
-            <Receipt />
-          </IconButton>
-        </DrawerNavLink>
-        <DrawerNavLink to='/lets-play'>
-          <IconButton onClick={closeDrawer}>
-            <VideogameAsset />
-          </IconButton>
-        </DrawerNavLink>
-      </Drawer>
+    <div className={getClassName()}>
+      {props.children}
     </div>
   )
+}
+
+EmptyNavItem.propTypes = {
+  activeClassName: string,
+  children: PropTypes.node.isRequired,
+  className: string,
+  to: string
+}
+
+export class NavBar extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      open: false,
+      activeNestedItem: false
+    }
+    this.closeDrawer = this.closeDrawer.bind(this)
+  }
+
+  transactionsBasePath = '/transactions'
+
+  mergeState (newState) {
+    // trying to avoid using some kind of state management library, possibly to my own detriment at this point
+    // it won't be too hard to add that in later, even if more features are added. in the meantime, need to
+    // merge in the existing state with the new state.
+    this.setState(merge(this.state, newState))
+  }
+
+  toggleDrawer () {
+    this.mergeState({ open: !this.state.open })
+  }
+
+  closeDrawer () {
+    this.mergeState({ open: false, activeNestedItem: '' })
+  }
+
+  getNestedListItems () {
+    return this.nestedListItems
+  }
+
+  setNestedListItems = (items) => { this.nestedListItems = items }
+
+  render () {
+    return (
+      <div>
+        <Drawer anchor='left' classes={{ paper: ('drawer') }} css={NavBarStyle} variant='permanent'>
+          <Icon
+            css={css`
+            width: 100% !important;
+            height: 50px !important;
+            margin-top: 10px !important;;
+            margin-bottom: 100px !important;
+          `}
+          >
+            <img
+              alt='Yer a Harry Wizard'
+              css={css`
+              padding: 12px !important;
+              width: 100%;
+            `}
+              src={WizardHatLogo}
+            />
+          </Icon>
+          <DrawerNavLink exact to='/'>
+            <IconButton onClick={this.closeDrawer}>
+              <Home />
+            </IconButton>
+          </DrawerNavLink>
+          <EmptyNavItem activeClassName='active-route' className={`${this.state.activeNestedItem === 'transactions' ? 'active-menu-item' : ''}`} to={this.transactionsBasePath} >
+            <IconButton onClick={() => {
+              if (!this.state.open) {
+                this.mergeState({ open: true, activeNestedItem: 'transactions' })
+              } else {
+                this.mergeState({ open: false, activeNestedItem: '' })
+              }
+            }}>
+              <Receipt />
+            </IconButton>
+          </EmptyNavItem>
+          <DrawerNavLink to='/lets-play'>
+            <IconButton onClick={this.closeDrawer}>
+              <VideogameAsset />
+            </IconButton>
+          </DrawerNavLink>
+          <div className='bottom-nav-container'>
+            <DrawerNavLink to='/settings'>
+              <IconButton onClick={this.closeDrawer}>
+                <Settings />
+              </IconButton>
+            </DrawerNavLink>
+          </div>
+        </Drawer>
+        {/* Can add in as many drawers as needed, with some refactoring to make their classes more specific */}
+        <Drawer anchor='left' classes={{ paper: ((this.state.open && 'nested-drawer-open') || 'nested-drawer') }} css={NavBarStyle} open={this.state.open} variant='permanent'>
+          <div className='nested-navigation-container'>
+            <div style={{ padding: '10px', textAlign: 'center' }}>Transactions</div>
+            <div className='nested-navigation-links'>
+              <NestedNavLink exact onClick={this.closeDrawer} to={`${this.transactionsBasePath}/history`}>
+                History
+              </NestedNavLink>
+              <NestedNavLink exact onClick={this.closeDrawer} to={`${this.transactionsBasePath}/add-transactions`}>
+                Add transaction
+              </NestedNavLink>
+            </div>
+          </div>
+        </Drawer>
+      </div>
+    )
+  }
 }
